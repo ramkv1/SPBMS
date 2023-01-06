d150b314b0107fe034e36099528105abadcd4f61
Ramakrishna <ramkv44@gmail.com>
2023-01-06 22:53:58 +0530

    MiniProj

---
+++ BootRestProj11-MiniProject-CURDOperation/src/main/java/com/rk/service/IDoctorMgmtService.java
@@ -0,0 +1,17 @@
+package com.rk.service;
+
+import java.util.List;
+
+import com.rk.entity.Doctor;
+import com.rk.exception.DoctorNotFoundException;
+
+
+public interface IDoctorMgmtService {
+	public String registerDoctor(Doctor doctor);
+	public List<Doctor> getAllDoctors();
+	public Doctor getDoctorById(int id) throws DoctorNotFoundException;
+	public String updateDoctor(Doctor doctor) throws DoctorNotFoundException;
+	public String deleteDoctorById(int docId) throws DoctorNotFoundException;
+	public String deleteDoctorsByIncomeRange(double start,double end);
+	public String hikeDoctorIncomeByPercentage(int docid,double percentage) throws DoctorNotFoundException;
+}
